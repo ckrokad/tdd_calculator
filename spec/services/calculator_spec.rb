fd8943f7b470667a78069566bfb8765043b0fc0b
Rokad <chintanrokad932@gmail.com>
2025-03-29 11:26:48 +0530

    red test added for new line

--- spec/services/calculator_spec.rb
+++ spec/services/calculator_spec.rb
@@ -30,5 +30,12 @@ RSpec.describe Calculator do
         expect(result).to eq(13)
       end
     end
+
+    context 'when input string contain new line char' do
+      it 'return correct sum' do
+        result = described_class.new.add("1\n2,3")
+        expect(result).to eq(6)
+      end
+    end
   end
 end
